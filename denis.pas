c2b147b24e4596f2eec48a946b41fc053ccf7a0a
Alexander Volkov <agvolkov5@gmail.com>
2019-02-22 14:01:01 +0300

    automat added; add collector of identifiers and constantns with types recognition

---
+++ denis.pas
@@ -0,0 +1,11 @@
+Program    example;
+{ sdfgdsfg
+fdgsd }
+var a:Integer;
+    s:String;
+begin
+    a := -5e+11
+    a    := 2. +    .3 - * .3E-12;
+    s := '     sadf s  dgsgsg';
+
+end.
--- example.pas
+++ example.pas
@@ -1,20 +1,60 @@
 Program example;
+function factorial(n: integer): integer;
+procedure abss;
+begin
+    writeln();
+end;
 var
-	a    : integer;
-    s : string;
+    a: integer;
+begin
+  if n>1 then
+    factorial:=n*factorial(n-1)
+  else
+    factorial:=1;
+    end;
+var
+	a, b: integer;
+    abc: array [1..10+(1),5..6] of integer;
+    abc1: array [1 .. 3] of integer;
+    ccc: array ['a' .. 'b'] of integer;
+    c: real;
+    s, d: string;
+label iteration, out;
 begin
 	a := 0;
-    s := 'Yes, I am gonna be a      star! Aha-aha!';
+    s := (''+'Hello');
     s := s + ', world!';
+    c := 0.005;
+    d:='';
+    abc[11 + 0- 1,5]:=5+6;
 
         { comment }
 
 	iteration:
-	if a > 5 then
+	if (a>5+1)then
 		goto out
 	else
-		a := a + 1;
+		a :=a+1;
+        b:= -1* 1- 0;
+        s:='a'+'b';
 	goto iteration;
+    
+    c :=1.2e+76;
 
 	out:
 end.
+
+
+
+
+
+
+{function factorial(n: integer): integer;
+var
+    a: integer;
+begin
+  if n>1 then
+    factorial:=n*factorial(n-1)
+  else
+    factorial:=1;
+    end;}
