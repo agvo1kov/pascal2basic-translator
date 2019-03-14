a8f81ef048bdea4266828ac39517414d104abc10
Alexander Volkov <agvolkov4@yandex.ru>
2019-03-15 02:59:25 +0300

    lab3 sketch

--- nk.pas
+++ nk.pas
@@ -4,17 +4,39 @@ var b,c,d:integer;
 	a, e,f,g:real;
 	s1, s2, s3,s4:string;
 	super_array: array [1..3, 10..20] of integer;
-	z: real;
-	kk: array [1..2] of string;
-
-LABEL m;
+	LABEL m;
 
 const 
 	Pi = 3.14;
 	Alpha = 332.1;
 	Betta = 4;
 
+procedure test_1 (param1: integer);
+	var aa, bb:integer;
+		cc, dd, gg: real;
+	begin
+		aa:= 0;
+		bb:=aa;
+		cc:=aa+bb;
+	end;
+function test_2 (param2: real):real;
+var kk: array [1..2] of string;
+	ggg:integer;
+begin
+	kk[1]:='FKDLKFDLF dkfLKDf dkfdkf;._!3:.ei integer begin           ';
+	ggg:=12;
+	kk[2]:='';
+	test_2:=ggg+0.5;
+end;
 
+{sdfsdf
+	commsenrepkfsf
+fdfsf
+
+
+fdf
+fdf
+}
 
 begin
 a := 5e+11;
